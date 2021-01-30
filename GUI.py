0f98afeb8598b68c2fcd5430b9d1809d8acc4b78
Emre Cenk <62028675+EmreCenk@users.noreply.github.com>
2021-01-30 15:57:05 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,7 +1,4 @@
 
-
-
-
 import tkinter as tk
 from gradient import GradientFrame
 
@@ -31,12 +28,12 @@ class page_displayer:
 
 
         self.main_frame = GradientFrame(self.root, from_color="#000000", to_color="#E74C3C", height=1000)
-
+        self.information={}
         # Placing the background:
         self.main_frame.place(relx=0.5, rely=0, relwidth=1, relheight=1, anchor="n")
 
         # PLACE HOLDER VALUES:
-        self.root.bind("<Configure>", self.on_resize)
+        # self.root.bind("<Configure>", self.on_resize)
 
         self.list_of_objects = []  # we will store a list of objects so that we can delete things on the screen
         self.list_of_attributes_to_resize=[]
@@ -44,12 +41,15 @@ class page_displayer:
 
         self.screen_index=0
         self.screen_order = [self.welcome_screen,
-                        self.question1]
-        pad = 3
-        self._geom = '200x200+0+0'
-        self.root.geometry("{0}x{1}+0+0".format(
-            self.root.winfo_screenwidth() - pad, self.root.winfo_screenheight() - pad))
-        self.root.bind('<Escape>', self.toggle_geom)
+                        self.question1,self.question2,self.question3]
+
+        # pad = 3
+        # self._geom = '200x200+0+0'
+        # self.root.geometry("{0}x{1}+0+0".format(
+        #     self.root.winfo_screenwidth() - pad, self.root.winfo_screenheight() - pad))
+        # self.root.bind('<Escape>', self.toggle_geom)
+
+        self.root.bind("<Button-1>", self.mouse_clicked)
 
     def toggle_geom(self, event):
         #Does not work lol
@@ -64,7 +64,7 @@ class page_displayer:
         self.list_of_text_objects=[]
 
     def create_proper_text(self,relx,rely,text,fill,font_tuple):
-        print(self.WIDTH,self.HEIGHT)
+
         x=relx*self.WIDTH
         y=rely*self.HEIGHT
         params=[x,y,text,fill,font_tuple]
@@ -72,6 +72,7 @@ class page_displayer:
         self.list_of_text_objects.append(created)
         self.list_of_attributes_to_resize.append([x,y,text,fill,font_tuple])
 
+        return created
     def on_resize(self, event_object):
 
         updated=[]
@@ -105,10 +106,11 @@ class page_displayer:
 
         self.list_of_objects = []  # Just to make sure everything is fully wiped from memory
 
-    def click_anywhere(self,size=20):
-        self.create_proper_text(0.5,0.8,"Click anywhere to continue","white",(font_name,size))
+    def click_anywhere(self,text="Click anywhere to continue",size=20):
+        self.click_text=self.create_proper_text(0.5,0.8,text,"white",(font_name,size))
 
     def question1(self):
+        self.screen_index=2
         self.clear_page()
         self.clear_text()
         self.create_proper_text(relx=0.3,rely=0.35,
@@ -117,19 +119,75 @@ class page_displayer:
 
 
         self.click_anywhere()
+    def question2(self):
+
+        self.clear_page()
+        self.clear_text()
+        alpha=self.create_proper_text(relx=0.3,rely=0.35,
+                                text="  How old are you?",
+                                fill="White",font_tuple=(font_name,80))
+
+        self.entry = tk.Entry(None,  # Since this will be the child of the frame
+
+                              font=(font_name,20),  # helvetica is the font, 60 is the font size
+                              )
+        self.entry.place(anchor="w", relx=0.2, rely=0.6, relheight=0.1, relwidth=0.15)  # Input field
+
+        self.click_anywhere()
+        self.screen_index=3
+
+    def question3(self):
+
+        self.clear_page()
+        self.clear_text()
+        alpha=self.create_proper_text(relx=0.5,rely=0.35,
+                                text="  What is your height in cm?",
+                                fill="White",font_tuple=(font_name,80))
+
+        self.entry = tk.Entry(None,  # Since this will be the child of the frame
+
+                              font=(font_name,20),  # helvetica is the font, 60 is the font size
+                              )
+        self.entry.place(anchor="w", relx=0.2, rely=0.6, relheight=0.1, relwidth=0.15)  # Input field
+
+        self.click_anywhere()
+        self.screen_index=4
+
     def mouse_clicked(self,even_object):
-        self.screen_order[self.screen_index]()
+
+        self.main_frame.itemconfig(self.click_text, text="  Please enter an integer\nClick anywhere to continue")
+        if self.screen_index in [3,4]:
+
+            potential_age=self.entry.get()
+            if potential_age=="":
+                potential_age="as"
+            try:
+
+                potential_age=int(potential_age)
+                self.information["age"]=potential_age
+                self.screen_order[self.screen_index]()
+            except:
+                pass
+
+            print(potential_age)
+
+        else:
+            try:
+                self.screen_order[self.screen_index]()
+            except:
+                pass
 
 
     def welcome_screen(self,):
-        self.screen_index+=1
+        self.screen_index=1
         self.create_proper_text(0.5,0.5,"Welcome to Fit Finder","white",(font_name,25))
         self.create_proper_text(0.5,0.4,"Hello.","White",(font_name,25))
 
         self.click_anywhere()
-        self.root.bind("<Button-1>", self.mouse_clicked)
-    
+
 from gradient import GradientFrame
 self=page_displayer()
 self.welcome_screen()
+
+
 self.screen.mainloop()
